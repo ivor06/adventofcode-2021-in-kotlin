89e7236de4edefab78c3a68020b27c5f5d29eaa9
ivor06 <ivor06@yandex.ru>
2021-12-01 15:11:15 +0000

    [AOC-2021] Add Day 1 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,10 +1,25 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val result = 1
+
+        for (i in 1..input.lastIndex)
+            if (input[i] > input[i - 1])
+                result.inc()
+
+        return result
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var firstNumber = input[0]
+        var result = 0
+
+        for (i in 3..input.lastIndex) {
+            if (input[i] > firstNumber)
+                result++
+            firstNumber = input[i - 2]
+        }
+
+        return result
     }
 
     // test if implementation meets criteria from the description, like:
